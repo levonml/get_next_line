834253320ac51e260b65cc31ed577b356a80b77a
lstepany <lstepany@hive.helsinki.fi>
2020-07-12 21:38:00 +0200

    printing single line per call

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lstepany <lstepany@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 09:23:07 by lstepany          #+#    #+#             */
-/*   Updated: 2020/07/11 22:59:49 by lstepany         ###   ########.fr       */
+/*   Updated: 2020/07/12 20:17:03 by lstepany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -19,10 +19,10 @@ int	get_next_line(const int fd, char **line)
 	int		i;
     int 	ret;
 	char 	buf[BUFF_SIZE];
-	char *str;
-	static	char *store;
-	str = "";
-	store = "";
+	static char *str = "";
+	static	char *store = "";
+	//	str = "";
+	//	store = "";
 	while((ret = read(fd, buf, BUFF_SIZE + 1))) 
 	{
 		buf[ret] = '\0';
