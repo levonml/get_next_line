2264375a1864228f3a4bb69a8869af778320e11e
lstepany <lstepany@student.hive.fi>
2020-07-24 18:18:24 +0300

    copy of clone gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,42 +6,80 @@
 /*   By: lstepany <lstepany@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 09:23:07 by lstepany          #+#    #+#             */
-/*   Updated: 2020/07/13 18:07:13 by lstepany         ###   ########.fr       */
+/*   Updated: 2020/07/24 15:50:49 by lstepany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/uio.h>
 #include <unistd.h>
 #include "libft.h"
+#include <stdlib.h>
 #include "get_next_line.h"
 
-int	get_next_line(const int fd, char **line)
+int		ft_newline(char *str)
 {
-	static int 		j = 0;
-	static	int		i = 0;
-    int				ret;
+	int				j;
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		if (str[j] == '\n')
+			return (j);
+		j++;
+	}
+	return (-1);
+}
+
+int		read_all(const int fd, char **store)
+{
 	char			buf[BUFF_SIZE + 1];
-	static  char	*str = "";
-   	char	*store;
+	int				ret;
+	char			*temp;
+
+	if (!(*store))
+		*store = ft_strnew(0);
 	if (fd < 0)
 		return (-1);
-	while((ret = read(fd, buf, BUFF_SIZE))) 
+	while ((ret = read(fd, buf, BUFF_SIZE)) && buf[ret] != EOF)
 	{
-				buf[ret] = '\0';		
-		str = ft_strjoin(str, buf);
+		if (ret < 0)
+			return (-1);
+		buf[ret] = '\0';
+		temp = ft_strjoin(*store, buf);
+		free(*store);
+		*store = ft_strdup(temp);
+		free(temp);
+		if (ft_strchr(buf, '\n'))
+			break ;
 	}
-	while (str[i] != '\n')
-		i++;
-	i++;
-	store = ft_strsub(str, j, i-j-1);
-	*line = store;
-	j = ft_strlen(store) + j;
-	if (str[i])
-		return (1);
-	if (!str[i])
-		return (0);
-	else return (-1);
-
 	return (0);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static	char	*store;
+	char			*temp;
+
+	if (read_all(fd, &store) < 0)
+		return (-1);
+	if ((store) && ft_newline(store) != -1)
+	{
+		*line = ft_strsub(store, 0, ft_newline(store));
+		temp = ft_strdup(ft_strchr(store, '\n') + 1);
+		free(store);
+		store = temp;
+		return (1);
+	}
+	if (!(*(store)))
+		return (0);
+	if (store == NULL)
+		return (-1);
+	if (ft_newline(store) == -1)
+	{
+		*line = ft_strdup(store);
+		free(store);
+		store = NULL;
+		return (1);
+	}
+	return (-1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: lstepany <lstepany@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 09:19:05 by lstepany          #+#    #+#             */
-/*   Updated: 2020/07/12 23:21:11 by lstepany         ###   ########.fr       */
+/*   Updated: 2020/07/24 12:58:07 by lstepany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE
-#define GET_NEXT_LINE
-#define BUFF_SIZE 10
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
 
 int get_next_line(const int fd, char **line);
 
